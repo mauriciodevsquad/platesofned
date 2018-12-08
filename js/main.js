12e8a7dbdacb35bab47680d1d3c52f2d263bf5ac
mauricio@devsquad.com <mauricio@devsquad.com>
2018-12-08 13:26:12 -0700

    Changed the service worker

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker
-            .register('../sw_cached_site.js')
+            .register('../sw_cached_pages.js')
             .then(reg => console.log('Service Worker: Registered'))
             .catch(err => console.log(`Service Worker: Error: ${err}`));
     });
