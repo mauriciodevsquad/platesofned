1efdac2684be144d5321806bc745cc968aaf8fc2
mauricio@devsquad.com <mauricio@devsquad.com>
2018-12-08 13:48:32 -0700

    Finally fixed the service worker paths :)

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker
-            .register('/platesofned/sw_cached_pages.js')
+            .register('/platesofned/sw_cached_site.js')
             .then(reg => console.log('Service Worker: Registered'))
             .catch(err => console.log(`Service Worker: Error: ${err}`));
     });
